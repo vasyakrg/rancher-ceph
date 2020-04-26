cf138776ab73d2beba15673d08d511d5e0eded03
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-04-26 16:07:04 +0700

    run once fdisk

--- 3.Ceph/3-run-ceph-server.sh
+++ 3.Ceph/3-run-ceph-server.sh
@@ -25,8 +25,8 @@ ceph-deploy mgr create node-1 node-2 node-3
 # get status from node-1
 ceph status
 
-# get disks from all nodes
-fdisk -l
+# get disks from node-1
+ssh root@node-1 'fdisk -l' && ssh root@node-2 'fdisk -l' && ssh root@node-3 'fdisk -l'
 
 # start OSD from node-1
 ceph-deploy osd create --data /dev/sdb node-1
