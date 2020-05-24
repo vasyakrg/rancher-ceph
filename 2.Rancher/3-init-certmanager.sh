08b452e3cc6512c72f7b210a37c2a3b5c5bb4f75
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-05-24 13:03:56 +0700

    add certnamanager fix

--- 2.Rancher/3-init-certmanager.sh
+++ 2.Rancher/3-init-certmanager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the CustomResourceDefinition resources separately
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.14/deploy/manifests/00-crds.yaml
 
 # Create the namespace for cert-manager
 kubectl create namespace cert-manager
@@ -13,7 +13,12 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
 # Install the cert-manager Helm chart
-helm install \
+helm upgrade --install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
-  --version v0.13.0
+  --version v0.15.0
+
+kubectl -n cert-manager rollout status deploy/cert-manager
+
+kubectl apply -f certmanager/certmanager-deploy.yaml
+kubectl apply -f certmanager/cluster-issuer.yaml
--- 2.Rancher/4-rancher-cluster.sh
+++ 2.Rancher/4-rancher-cluster.sh
@@ -2,7 +2,27 @@
 
 # create and set domain name `rke.amegaserver.com` in DNS-servers BEFORE!
 
+HOST_NAME=rke.amegaserver.com
+LETS_EMAIL=vasyakrg@gmail.com
+
 kubectl create namespace cattle-system
 
+kubectl -n cattle-system create secret tls tls-rancher-ingress \
+  --cert=tls/tls.crt \
+  --key=tls/tls.key
+
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=rke.amegaserver.com --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=vasyakrg@gmail.com
+
+# with Lets
+# helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=${HOST_NAME} --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=${LETS_EMAIL}
+
+# with custom CA
+
+helm install rancher-latest/rancher \
+  --name rancher \
+  --namespace cattle-system \
+  --set hostname=${HOST_NAME} \
+  --set ingress.tls.source=secret
+  --set privateCA=true
+
+kubectl -n cattle-system rollout status deploy/rancher
